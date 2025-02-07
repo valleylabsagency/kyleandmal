315629c9bba6cb09d8b4cca01eb46b0217ec6a70
Tyler Valley <tylervalley@Tylers-MacBook-Pro-2.local>
2025-02-07 08:36:20 -0500

    app.js edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
